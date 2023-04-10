ab156c41ed9eb211a1a33b65c55871057298a3c3
juanPablo96 <juan.pablo1996@hotmail.com>
2023-04-10 16:03:07 -0300

    pokedex styles

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "./Card.css";
 
 function Card(props) {
   const pokemon = props.pokemon;
   return (
-    <div className="containerCard">
-      <p>{pokemon.Number}</p>
-      <img src={pokemon.Image} alt="Pokemon" />
-      <h3>{pokemon.Name}</h3>
-    </div>
+    <section className="gContainer">
+      <div className="containerCard">
+        <p className="pokemonNumber">{pokemon.Number} </p>
+        <img src={pokemon.Image} alt="Pokemon" className="imgCard" />
+        <h3 className="pokemonName">{pokemon.Name}</h3>
+      </div>
+    </section>
   );
 }
 
